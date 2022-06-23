a11821c43f45cd4c88877674ba379436ecc8e67d
juaoc <juaoc@10.10.100.117>
2022-06-22 21:55:34 -0300

    comit

--- poo-entra21/src/principal/MinhaPrimeiraClasseJava.java
+++ poo-entra21/src/principal/MinhaPrimeiraClasseJava.java
@@ -4,7 +4,7 @@ public class MinhaPrimeiraClasseJava {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String texto = "turma 2 ";
+		String texto = "turma 2 segundo teste ";
 		
 		System.out.println("ola " + texto);
 
