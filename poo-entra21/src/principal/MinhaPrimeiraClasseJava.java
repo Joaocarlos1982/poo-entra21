319a37c4522ddec0b53850a2b8b6b1c6e4f209fd
juaoc <juaoc@10.10.100.117>
2022-06-22 21:46:26 -0300

    primeiro commit

---
+++ poo-entra21/src/principal/MinhaPrimeiraClasseJava.java
@@ -0,0 +1,13 @@
+package principal;
+
+public class MinhaPrimeiraClasseJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String texto = "turma 2 ";
+		
+		System.out.println("ola " + texto);
+
+	}
+
+}
